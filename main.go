62ebc383aa78afafec5a6975f5e6272dde4546ec
Варламов Алексей Сергеевич <AVarlamov2@alfabank.ru>
2022-04-20 10:38:42 +0300

    first commit

--- main.go
+++ main.go
@@ -1,1 +1,23 @@
-package awesomeProject
+package main
+
+import (
+	"fmt"
+	"math/rand"
+)
+
+//type int
+func expression() bool {
+	x := rand.Intn(10)
+	return x != 5
+
+}
+
+func main() {
+
+	//var a int
+	for expression() {
+		//a := 0
+		fmt.Println("Hello World")
+
+	}
+}
