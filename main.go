c6dc32bf60c9f64809c3f2070cff213e14a1eaab
Варламов Алексей Сергеевич <AVarlamov2@alfabank.ru>
2022-04-20 11:33:29 +0300

    1:21:43 func

--- main.go
+++ main.go
@@ -2,18 +2,10 @@ package main
 
 import "fmt"
 
-//type int
-//func expression() bool {
-//	x := rand.Intn(10)
-//	return x != 5
-//
-//}
+func HelloWorld() {
+	fmt.Println("Hello WOrld")
+}
 
 func main() {
-	var a, b int
-	a = 5
-	b = 100
-	_ = b
-	fmt.Println(a)
-
+	HelloWorld()
 }
