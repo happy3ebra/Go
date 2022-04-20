cb2c4d5843223a011203076a64ec113b13940df8
Варламов Алексей Сергеевич <AVarlamov2@alfabank.ru>
2022-04-20 11:54:59 +0300

    1:33:23 func+arg

--- main.go
+++ main.go
@@ -3,8 +3,7 @@ package main
 import "fmt"
 
 func HelloWorld(prefix string, name ...string) string {
-	fmt.Println("Hello WOrld", name)
-	return fmt.Sprintf("%s Hello W0rld %s ", name)
+	return fmt.Sprintf("%s Hello W0rld %s ", prefix, name)
 }
 
 func main() {
