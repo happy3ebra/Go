ed0fce2a257c24afe3baf53f0f5089eb496938c0
Варламов Алексей Сергеевич <AVarlamov2@alfabank.ru>
2022-04-20 10:51:12 +0300

    51:03

--- main.go
+++ main.go
@@ -15,6 +15,13 @@ func main() {
 
 	//var i int
 	//rand.Seed(time.Now().Unix())
+	j := 0
+	for j < 5 {
+		//a := 0
+		fmt.Println("Hello World", j)
+		j++
+
+	}
 	for i := 0; i < 5; i++ {
 		//a := 0
 		fmt.Println("Hello World", i)
