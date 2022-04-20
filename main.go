5d746da96f17e7128c810f7a0c3a3e32fd148fec
Варламов Алексей Сергеевич <AVarlamov2@alfabank.ru>
2022-04-20 11:09:08 +0300

    1:05:03 massive perebor

--- main.go
+++ main.go
@@ -13,7 +13,7 @@ func main() {
 
 	//var i int
 	//rand.Seed(time.Now().Unix())
-	x := [3]int{0, 1, 2}
+	x := [9]int{0, 167, 267, 5656, 5657, 555, 555, 6567567, 777}
 	for i := 0; i < len(x); i++ {
 		fmt.Println(x[i])
 	}
